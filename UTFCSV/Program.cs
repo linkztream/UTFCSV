1edb0e0446b5ccbad525f1ca9e4dce8c05c439ad
Pär Lindström <linkztream@gmail.com>
2021-05-20 11:20:17 +0200

    Add project files.

---
+++ UTFCSV/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace UTFCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return; // Ingen fil droppad så slutar vi
+            string text = File.ReadAllText(args[0]);
+
+            text = "sep=,\n" + text;
+
+            // Sno namn och sökväg från filen som blev droppad och skapa en ny
+            string path = Path.GetDirectoryName(args[0])
+                + Path.DirectorySeparatorChar
+                + Path.GetFileNameWithoutExtension(args[0])
+                + "_reCSV" + Path.GetExtension(args[0]);
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding destEnc = Encoding.GetEncoding(1250);
+            File.WriteAllText(path, text, destEnc);
+
+            Console.WriteLine("Hit any key to exit...");
+            Console.ReadKey();
+            return;
+
+        }
+    }
+}
